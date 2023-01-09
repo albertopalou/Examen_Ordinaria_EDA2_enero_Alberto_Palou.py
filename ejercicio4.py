bbad93ab86ffd9b22a6225d0a7fc191d71600365
albertopalou <alberto.palouorellanapizarro@usp.ceu.es>
2023-01-09 17:09:04 +0100

    guardar

--- ejercicio4.py
+++ ejercicio4.py
@@ -8,8 +8,10 @@ class ArtefactosValiosos:
     
     def __str__(self):
         print("La conserva se ha creado con exito")
-        return "El objeto {}, tiene un peso de {} kg, un precio de {} euros, y su fecha de caducidad es el {}" .format(self.nombre, self.peso, self.precio, self.caducidad)
+        return "El artefacto {}, tiene un peso de {} kg, un precio de {} euros, y su fecha de caducidad es el {}" .format(self.nombre, self.peso, self.precio, self.caducidad)
 
 artefacto1 = ArtefactosValiosos("3", "reloj", "300", "12-09-2030")
 artefacto2 = ArtefactosValiosos("70", "mueble", "550", "22-03-2028")
 artefacto3 = ArtefactosValiosos("100", "espejo", "180", "01-04-2040")
+
+print(artefacto1)
