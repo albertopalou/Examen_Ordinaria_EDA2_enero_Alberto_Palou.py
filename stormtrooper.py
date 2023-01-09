b8d6bf7e9b1897a8d7e0f86a0ba6c93cf5f4c801
albertopalou <alberto.palouorellanapizarro@usp.ceu.es>
2023-01-09 16:54:08 +0100

    guardar

--- stormtrooper.py
+++ stormtrooper.py
@@ -8,17 +8,22 @@ class Stormtrooper:
         return "El stormtrooper {} se ha creado con exito" . format(self.nombre)
     
 class Calificacion(Stormtrooper):
-    def __init__(self, codigo, cohoerte, siglo, escuadra, numero):
+    def __init__(self,nombre , rango, codigo, cohoerte, siglo, escuadra, numero):
+        self.nombre = nombre
+        self.rango = rango
         self.codigo = codigo
         self.cohoerte = cohoerte
         self.siglo = siglo
         self.escuadra = escuadra
         self.numero = numero
+    
+    def __str__(self):
+        return "El solado {} de rango {}: {} - {}{}{}{}" . format(self.nombre, self.rango, self.codigo, self.cohoerte, self.siglo, self.escuadra, self.numero)
 
-soldado1 = ["GP", "4", "3", "9", "3"]
-soldado2 = ["TK", "2", "2", "7", "1"]
-soldado3 = ["JL", "3", "9", "4", "5"]
+soldado1 = Calificacion("Thomas", "Sargento", "GP", "4", "3", "9", "3")
+soldado2 = Calificacion("John", "Capitan", "CV", "4", "3", "9", "3")
+soldado3 = Calificacion("Andrew", "Solado raso", "JK", "3", "9", "4", "5")
 
 lista = [soldado1, soldado2, soldado3]
 
-print(Stormtrooper.soldado1())
+print(Calificacion.lista())
