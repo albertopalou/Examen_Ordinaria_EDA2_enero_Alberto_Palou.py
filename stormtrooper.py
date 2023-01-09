1ef8d29c9ee8914363288d480e6fdbbfe820357e
albertopalou <alberto.palouorellanapizarro@usp.ceu.es>
2023-01-09 16:38:18 +0100

    guardar

--- stormtrooper.py
+++ stormtrooper.py
@@ -6,3 +6,18 @@ class Stormtrooper:
     
     def __str__(self):
         return "El stormtrooper {} se ha creado con exito" . format(self.nombre)
+    
+    def calificacion(self, codigo, cohoerte, siglo, escuadra, numero):
+        self.codigo = codigo
+        self.cohoerte = cohoerte
+        self.siglo = siglo
+        self.escuadra = escuadra
+        self.numero = numero
+
+soldado1 = ["GP", "4", "3", "9", "3"]
+soldado2 = ["TK", "2", "2", "7", "1"]
+soldado3 = ["JL", "3", "9", "4", "5"]
+
+lista = [soldado1, soldado2, soldado3]
+
+print(Stormtrooper.soldado1())
